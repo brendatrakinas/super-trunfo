d94decddca21f12fc24d8eb62255a63dbc47fe70
brendatrakinas <fe.twist@gmail.com>
2022-02-20 19:43:29 -0300

    Initial commit

---
+++ dist/script.js
@@ -0,0 +1,378 @@
+var carta1 = {
+  nome: "Blu",
+  imagem:
+    "https://i.pinimg.com/564x/64/29/7c/64297c8465d706f2eefb37bb683eeb48.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 5,
+    curiosidade: 7,
+    especial: 8
+  }
+};
+var carta2 = {
+  nome: "Marquinhos",
+  imagem:
+    "https://i.pinimg.com/564x/30/6b/fc/306bfc4de4b8b27b67b44cded6738f64.jpg",
+  atributos: {
+    ataque: 3,
+    defesa: 9,
+    curiosidade: 8,
+    especial: 4
+  }
+};
+var carta3 = {
+  nome: "Silveirinha filho do dono",
+  imagem:
+    "https://i.pinimg.com/564x/ef/c2/44/efc244a3d384aeb75417650314c46033.jpg",
+  atributos: {
+    ataque: 3,
+    defesa: 5,
+    curiosidade: 8,
+    especial: 6
+  }
+};
+var carta4 = {
+  nome: "Mamma & Mia",
+  imagem:
+    "https://i.pinimg.com/564x/a7/36/fe/a736feb902d084582e9178dfc065e738.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 8,
+    curiosidade: 6,
+    especial: 3
+  }
+};
+var carta5 = {
+  nome: "Gerson Mendonça",
+  imagem:
+    "https://i.pinimg.com/564x/6d/1a/9f/6d1a9f4b8eab8e9ddb4bd0c6b6aabc47.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 9,
+    curiosidade: 7,
+    especial: 4
+  }
+};
+var carta6 = {
+  nome: "Fátima",
+  imagem:
+    "https://i.pinimg.com/564x/4d/55/e0/4d55e014b39beea00e43536f6e7a6ef3.jpg",
+  atributos: {
+    ataque: 4,
+    defesa: 8,
+    curiosidade: 5,
+    especial: 3
+  }
+};
+var carta7 = {
+  nome: "Mariah",
+  imagem:
+    "https://i.pinimg.com/564x/48/58/b9/4858b9e6107d3210ed4e25513d02a1e7.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 3,
+    curiosidade: 9,
+    especial: 8
+  }
+};
+var carta8 = {
+  nome: "Nickzinho café com leite",
+  imagem:
+    "https://i.pinimg.com/564x/88/fd/64/88fd643c44991ebc5d232e91ebe4a0f1.jpg",
+  atributos: {
+    ataque: 2,
+    defesa: 6,
+    curiosidade: 9,
+    especial: 8
+  }
+};
+var carta9 = {
+  nome: "Boa Vizinhança",
+  imagem:
+    "https://i.pinimg.com/564x/17/73/76/17737687a9665886d06b5f438b37cc6f.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 8,
+    curiosidade: 10,
+    especial: 4
+  }
+};
+var carta10 = {
+  nome: "Acoplada",
+  imagem:
+    "https://i.pinimg.com/564x/c5/cc/ce/c5ccce1de09cc846fc97dca6a503387c.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 8,
+    curiosidade: 4,
+    especial: 5
+  }
+};
+var carta11 = {
+  nome: "Jura",
+  imagem:
+    "https://i.pinimg.com/564x/12/35/f5/1235f59525073681fc0ceaf99e50dcd8.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 6,
+    curiosidade: 5,
+    especial: 9
+  }
+};
+var carta12 = {
+  nome: "xameguinho",
+  imagem:
+    "https://i.pinimg.com/564x/5e/6f/a0/5e6fa07abc0c539c40a8f503bbd4324c.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 8,
+    curiosidade: 6,
+    especial: 8
+  }
+};
+var carta13 = {
+  nome: "Diego da papelaria",
+  imagem:
+    "https://i.pinimg.com/564x/db/86/ad/db86adccaa6207e8659d0fd572a72663.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 6,
+    curiosidade: 6,
+    especial: 7
+  }
+};
+var carta14 = {
+  nome: "Kat moss",
+  imagem:
+    "https://i.pinimg.com/564x/b9/fd/3f/b9fd3f4683b207086f9bada8ffee01db.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 7,
+    curiosidade: 5,
+    especial: 8
+  }
+};
+var carta15 = {
+  nome: "zeca jornaleiro",
+  imagem:
+    "https://i.pinimg.com/564x/58/d2/a9/58d2a90f6701b9c42ae02f785c79ac64.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 7,
+    curiosidade: 8,
+    especial: 9
+  }
+};
+var carta16 = {
+  nome: "pedágio",
+  imagem:
+    "https://i.pinimg.com/564x/89/d1/28/89d128218614f50146b390fd9ee69bc8.jpg",
+  atributos: {
+    ataque: 4,
+    defesa: 10,
+    curiosidade: 6,
+    especial: 8
+  }
+};
+var carta17 = {
+  nome: "big boi",
+  imagem:
+    "https://i.pinimg.com/564x/56/66/8e/56668e277eb8ec3a62f6bd744d3b53b8.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 5,
+    curiosidade: 7,
+    especial: 9
+  }
+};
+var carta18 = {
+  nome: "so fresh so clean",
+  imagem:
+    "https://i.pinimg.com/564x/ab/5e/f6/ab5ef63f5d4fa4f19ff20971c32f1877.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 4,
+    curiosidade: 9,
+    especial: 8
+  }
+};
+var carta19 = {
+  nome: "kevin bebe",
+  imagem:
+    "https://i.pinimg.com/564x/79/6d/ac/796dac71b18e8ec86aa158a1286781be.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 5,
+    curiosidade: 7,
+    especial: 9
+  }
+};
+var carta20 = {
+  nome: "tipo dono",
+  imagem:
+    "https://i.pinimg.com/564x/84/7c/c8/847cc86d49e323c2bebbe770f9a6498c.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 5,
+    curiosidade: 5,
+    especial: 7
+  }
+};
+var carta21 = {
+  nome: "urubu vigilante",
+  imagem:
+    "https://i.pinimg.com/564x/95/40/ad/9540adfe287c4ae10c5da38865a88ba1.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 4,
+    curiosidade: 9,
+    especial: 8
+  }
+};
+var carta22 = {
+  nome: "sumido",
+  imagem:
+    "https://i.pinimg.com/564x/f8/58/33/f858337ea20fa2f1f831845166d155bc.jpg",
+  atributos: {
+    ataque: 4,
+    defesa: 8,
+    curiosidade: 7,
+    especial: 8
+  }
+};
+
+
+var cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20,
+  carta21,
+  carta22
+];
+
+var CartaMaquina;
+var cartaJogador;
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * 22);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * 22);
+  while (numeroCartaMaquina == numeroCartaJogador) {
+    numeroCartaJogador = parseInt(Math.random() * 22);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+  console.log(cartaJogador);
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributo = document.getElementsByName("atributo");
+  for (var i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked) {
+      return radioAtributo[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divResultado = document.getElementById("resultado");
+  // var elementoResultado = document.getElementById("resultado")
+  // var valorCartaJogador = cartaJogador.atributos[atributoSelecionado]
+  // var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado]// ;
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Venceu</p>";
+    // htmlResultado = "<p class='resultado-final'>Venceu</p>";
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Perdeu</p>";
+  } else {
+    htmlResultado = "<p class='resultado-final'>Empatou</p>";
+  }
+  divResultado.innerHTML = htmlResultado;
+  document.getElementById("btnJogar").disabled = true;
+  exibirCartaMaquina();
+  
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  //  divCartaJogador.style.backgroundImage = "url("+ cartaJogador.imagem +")"
+  var moldura =
+    '<img src="https://fernagarcia.files.wordpress.com/2022/02/bodegacats_card-2.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle"> ${cartaJogador.nome}</p>`;
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  var moldura =
+    '<img src="https://fernagarcia.files.wordpress.com/2022/02/bodegacats_card-2.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      "" +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle"> ${cartaMaquina.nome}</p>`;
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+// function jogarNovamente() {
+//  var atributoSelecionado = obtemAtributoSelecionado();
+ // for (var i = 0; i < cartas.length; i++) {
+}
+// function exibirOpcoes ()  {
+//   var opcoes = document.getElementById("opcoes")
+//   var opcoesTexto = ""
+//   for (var atributo in cartaJogador.atributos) {
+//     opcoesTexto = "<input type=´radio´ name= 'atributo'value='" + atributo + "'>";
+//   }
+//   opcoes.innerHTML = opcoesTexto;
+//  }
